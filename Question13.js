211809101cc8617fd321d57daff9dcd5d2610853
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-02-23 02:24:02 +0530

    Create README.md

---
+++ Question13.js
@@ -0,0 +1,9 @@
+// Branch Name: B2_W4_D3
+
+//  Question 13: Write a for-loop:
+- Start from 0th index, log every element,
+- then go to next index, log all next elements,
+- continue till last element
+
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]    
+
