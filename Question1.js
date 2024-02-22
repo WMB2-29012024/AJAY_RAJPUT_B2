69ebeacb579524fa894e668064c04e9d12808b7c
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-22 17:52:22 +0530

    Qustion 1

---
+++ Question1.js
@@ -0,0 +1,11 @@
+const numArr = [1, 234_45, 89,75, 4e9];
+for(let i=0;i<numArr.length;i++){
+    console.log(numArr[i]);
+}
+const strArr = ["asd", "qwe", "ert", "asdasdzxczxc"];
+for(let i=0;i<strArr.length;i++){
+    console.log(strArr[i]);
+}
+const mixArr = ["first", "234_45", {first: "Web", last: "Masters"}, true];
+for(let i=0;i<mixArr.length;i++){
+    console.log(mixArr[i]);}
