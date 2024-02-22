9afd5822f07ef864524e4e379cdaf551e20fa690
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-02-23 02:24:01 +0530

    Create README.md

---
+++ Question11.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W4_D3
+
+//  Question 11: Write a for-loop and count frequency of each element in the array. Store the element and its frequency in an object.
+
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]    
+
