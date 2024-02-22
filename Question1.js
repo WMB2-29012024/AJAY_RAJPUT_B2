61cbf53fd043aba8a9268e740424e54164d5e894
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-02-23 02:23:55 +0530

    Create README.md

---
+++ Question1.js
@@ -0,0 +1,8 @@
+// Branch Name: B2_W4_D3
+
+//  Question 1: Write a for loop and log each element, starting from 0th index.
+
+const numArr = [1, 234_45, 89,75, 4e9]
+const strArr = ["asd", "qwe", "ert", "asdasdzxczxc"]
+const mixArr = ["first", "234_45", {first: "Web", last: "Masters"}, true]
+
