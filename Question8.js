924648e7d0ccd50bcb7d3455032ac7c9beacdaf6
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-23 00:58:36 +0530

    Questiion 8

--- Question8.js
+++ Question8.js
@@ -1,18 +1,16 @@
 
 function currAndImmediateNext(arr){
 let store=[];
-
-for(let i=0;i<2;i++){
+for(let i=0;(i<arr.length-1);i++){
+    if(i===(arr.length-1)){
     store.push(arr[i]);
-    store.push(arr[i+1]);
+}
+    else{
+        store.push([arr[i],arr[i+1]]);
+}
 }
 return store;
 }
-
-
-
-
-
 const numArr = currAndImmediateNext([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     
 console.log(numArr);
