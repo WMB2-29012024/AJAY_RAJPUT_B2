01d893a089e304155eb9690119a51d5dbf7c0a75
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-02-23 02:24:00 +0530

    Create README.md

---
+++ Question10.js
@@ -0,0 +1,8 @@
+// Branch Name: B2_W4_D3
+
+//  Question 10: Write a for-loop, and log the current, its immediate previous, and its immediate next element,
+Put the elements in a new object, and push that object in an array.
+Return that array at the end of the loop
+
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]    
+
