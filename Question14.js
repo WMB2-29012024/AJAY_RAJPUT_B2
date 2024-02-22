67d2ea3e055cfcd4545106a867b8fb71ef0684d7
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-02-23 02:24:03 +0530

    Create README.md

---
+++ Question14.js
@@ -0,0 +1,7 @@
+// Branch Name: B2_W4_D3
+
+//  Question 14: There are 2 different arrays, write a for-loop, that should log elements at the same index in both the arrays. The loop should run till the longer array's length.
+
+const numArr1 = [1, 2, 3, 4, 5]
+const numArr2 = [6, 7, 8, 9, 10, 11, 12]    
+
