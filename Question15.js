2e693da2a2ae7cfe841c18e6a7279c50087e0122
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-02-23 02:24:03 +0530

    Create README.md

---
+++ Question15.js
@@ -0,0 +1,11 @@
+// Branch Name: B2_W4_D3
+
+//  Question 15: There are 2 different arrays, write a for-loop, that:
+starts from 0th index, on array one,
+matches all the elements of the array two,
+if any element matches, logs true, and continue to next element
+once all the elements are looped on array 2, go to next element in array 1, then continue from step 1.
+
+const numArr1 = [1, 2, 3, 4, 5]
+const numArr2 = [6, 7, 8, 9, 10, 11, 12]    
+
