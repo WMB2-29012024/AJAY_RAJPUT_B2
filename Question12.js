a6897c916f2a1161afd21be21008e6a7ed69a552
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-25 00:59:57 +0530

    Question 12 solution

--- Question12.js
+++ Question12.js
@@ -1,8 +1,7 @@
 ////B2_W4_D2
 
 
-//Question 11.  Log the values from the object in an array, using Object.values.
-
+//Question 12.  Log the keys from the object in an array, using Object.keys.
 let obj={ name:"ajay", age :22, email:"ajay@gmail.com", address:"Boda",pincode:465685};
-let k=Object.values(obj);
+let k=Object.keys(obj);
 console.log(k);
