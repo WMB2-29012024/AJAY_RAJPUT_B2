c536a633cfc016b43b102602bda54c61a5ce52a7
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-02-24 11:18:56 +0530

    Create README.md

---
+++ Question16.js
@@ -0,0 +1,8 @@
+// Branch Name: B2_W4_D5
+      
+/* Question 16: Write a for loop to find the index of a specific element in the array.
+*/
+
+const arr = ["apple", "banana", "orange", "grape"];
+const target = "orange";
+
