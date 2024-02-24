fe51ed333442ab0ebe6cee9a8d06f162dcf86737
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-02-24 11:18:56 +0530

    Create README.md

---
+++ Question15.js
@@ -0,0 +1,7 @@
+// Branch Name: B2_W4_D5
+      
+/* Question 15: Write a for loop to log elements that end with the letter "e"
+*/
+
+const strArr = ["banana", "apple", "orange", "grape", "kiwi"];
+
