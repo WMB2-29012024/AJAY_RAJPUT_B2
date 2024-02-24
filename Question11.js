516484952a66fee3c861e2c52acf2f49d69a9cbd
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-25 00:57:37 +0530

    Question 11 solution

---
+++ Question11.js
@@ -0,0 +1,8 @@
+////B2_W4_D2
+
+
+//Question 11.  Log the values from the object in an array, using Object values.
+
+let obj={ name:"ajay", age :22, email:"ajay@gmail.com", address:"Boda",pincode:465685};
+let k=Object.values(obj);
+console.log(k);
