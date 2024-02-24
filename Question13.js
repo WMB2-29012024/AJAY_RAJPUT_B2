1cb1836fa1faa322130e89bfde792e4d7a3b3fe5
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-02-24 11:18:55 +0530

    Create README.md

---
+++ Question13.js
@@ -0,0 +1,7 @@
+// Branch Name: B2_W4_D5
+      
+/* Question 13: Write a for loop to concatenate all elements into a single string.
+*/
+
+const strArr = ["banana", "apple", "orange", "grape", "kiwi"];
+
