4e571e69b2b1eaa311c389e74dd0ed3eb50592ed
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-25 01:18:14 +0530

    Question 15 solution

---
+++ Question15.js
@@ -0,0 +1,8 @@
+ ////B2_W4_D2
+
+
+//Question 15.  Delete the key “email” with its value from object, and log the object (square notation, dot notation any)
+
+let obj={ name:"ajay", age :22, email:"ajay@gmail.com",phone:"91444xxxxx", address:"Boda",pincode:465685};
+delete obj.email;
+console.log(obj);
