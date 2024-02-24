a08acaf6307a4ba19a12e68ef153106d3bfc6106
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-25 01:06:52 +0530

    Question 13 solution

--- Question10.js
+++ Question10.js
@@ -2,5 +2,5 @@
 
 
 //Question 10.  Create an object with following keys = “name”, “age”, “email”, “phone”, “address”, “pincode”. And add their corresponding values, and log the object.
-let obj={ name:"ajay", age :22, email:"ajay@gmail.com", address:"Boda",pincode:465685};
+let obj={ name:"ajay", age :22, email:"ajay@gmail.com",phone:"91444xxxxx", address:"Boda",pincode:465685};
 console.log(obj);
---
+++ Question13.js
@@ -0,0 +1,6 @@
+////B2_W4_D2
+
+
+//Question 13.  Log the value of key “phone” in the object (square notation, dot notation any).
+let obj={ name:"ajay", age :22, email:"ajay@gmail.com",phone:"91444xxxxx", address:"Boda",pincode:465685};
+console.log(obj.phone);
