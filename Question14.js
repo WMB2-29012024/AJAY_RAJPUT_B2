bc7077ae92ad6f873f8fe3b06b2152e47e6907c4
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-25 01:11:32 +0530

    Question 14 solution

---
+++ Question14.js
@@ -0,0 +1,8 @@
+////B2_W4_D2
+
+
+//Question 14.  Change the value of key “pincode” and log the new value (square notation, dot notation any).
+
+let obj={ name:"ajay", age :22, email:"ajay@gmail.com",phone:"91444xxxxx", address:"Boda",pincode:465685};
+obj.pincode=465686;
+console.log(obj.pincode);
