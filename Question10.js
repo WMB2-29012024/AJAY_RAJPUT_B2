e0465f0825eaf6aae0272cd5fd562aab591485f3
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-25 00:44:34 +0530

    Question 10 solution

---
+++ Question10.js
@@ -0,0 +1,6 @@
+////B2_W4_D2
+
+
+//Question 10.  Create an object with following keys = “name”, “age”, “email”, “phone”, “address”, “pincode”. And add their corresponding values, and log the object.
+let obj={ name:"ajay", age :22, email:"ajay@gmail.com", address:"Boda",pincode:465685};
+console.log(obj);
