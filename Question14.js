087e818a6521a39dffcc556bc8faacf4171140ed
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-02-24 11:18:56 +0530

    Create README.md

---
+++ Question14.js
@@ -0,0 +1,7 @@
+// Branch Name: B2_W4_D5
+      
+/* Question 14: Write a for loop to log elements that start with the letter "a".
+*/
+
+const strArr = ["banana", "apple", "orange", "grape", "kiwi"];
+
