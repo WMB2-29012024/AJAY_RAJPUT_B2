30f135a128a2e3fb44b986b5b83efb5befcfef42
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-25 16:12:46 +0530

    Question 20 solution

---
+++ Question20.js
@@ -0,0 +1,14 @@
+////B2_W4_D2
+
+
+//Question 20.  Create an array of objects, where each object represents a primitive data type in JS, its keys should be like Eg: {“dataTypeName”: “string”, “type”: “primitive”, “isObject”: false, “isMultipleValuesAllowed”: false}
+let arr=[{dataTypeName:"String", type:"primitive", isObject:"false", isMultipleValuesAllowed:"false"},
+{dataTypeName:"Boolean", type:"primitive", isObject:"false", isMultipleValuesAllowed:"false"},
+{dataTypeName:"BigInt", type:"primitive", isObject:"false", isMultipleValuesAllowed:"false"},
+{dataTypeName:"Number", type:"primitive", isObject:"false", isMultipleValuesAllowed:"false"},
+{dataTypeName:"Null", type:"primitive", isObject:"false", isMultipleValuesAllowed:"false"},
+{dataTypeName:"Undefined", type:"primitive", isObject:"false", isMultipleValuesAllowed:"false"},
+{dataTypeName:"Symbol", type:"primitive", isObject:"false", isMultipleValuesAllowed:"false"}];
+
+
+console.log(arr);
