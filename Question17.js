423ce84c0281ebd8e8492b2a67c13dc81c5aac0f
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-25 09:28:14 +0530

    Question 17 solution

---
+++ Question17.js
@@ -0,0 +1,6 @@
+////B2_W4_D2
+
+
+//Question 17.  Create an object with key “primitivetypes”, and value should be an array of strings containing names of all primitive data types in JS
+let obj={primitivetypes:[17,true, "ajay",755n, null, undefined,Symbol()]}; 
+console.log(obj);
