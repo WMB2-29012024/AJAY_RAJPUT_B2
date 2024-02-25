22f52795ce6c7ccb83ff1bbf2d53b149c5b8b043
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-25 20:15:58 +0530

    Question 21 solution

---
+++ Question21.js
@@ -0,0 +1,6 @@
+////B2_W4_D2
+
+
+//Question 21.  Create a map of all non-primitive data types in JS with name and their data type Eg: [[“array”, “non-primitive”], [“set”, “non-primitive”]]
+const obj= new Map([["array","non-primitive"],["object","non-primitive"],["map","non-primitive"],["set","non-primitive"],["class","non-primitive"],["date","non-primitive"],["weakset","non-primitive"],["weakmmap","non-primitive"],["JSON","non-primitive"]]);
+console.log(obj);
