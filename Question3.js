0649e04b096d21510b322707f71ee713f3ae29e3
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-26 11:28:03 +0530

    Question 3 Solution

--- Question3.js
+++ Question3.js
@@ -3,5 +3,12 @@
 /* Question 3: Write a for loop to find and log the largest element in the array.
 */
 
-const numArr = [10, 2, 30, 4, 50];
+const numArr = [10, 2, 30, 4, 50,4,50];
+let largestElement=0;
+for(let i =0; i<numArr.length;i++){
+if(largestElement<=numArr[i]){
+    largestElement=numArr[i];
+}
+}
+console.log(largestElement);
 
