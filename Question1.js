508685946c756de1cd083b49b40669d3687fd537
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-26 10:53:20 +0530

    Question 1 solution

--- Question1.js
+++ Question1.js
@@ -1,7 +1,15 @@
 // Branch Name: B2_W4_D5
-      
+
 /* Question 1: Write a for loop to log each element multiplied by 2.
 */
 
 const numArr = [1, 2, 3, 4, 5];
+const func = () => {
+    let newNumArr = [];
+    for (let i = 0; i < numArr.length; i++) {
+        newNumArr.push(2 * (numArr[i]));
 
+    }
+    console.log(newNumArr);
+}
+func();
