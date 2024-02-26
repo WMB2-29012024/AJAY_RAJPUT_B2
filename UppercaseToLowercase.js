4fec8b52e84960d964b55544f94170b8a94d0c2c
InvincibleAj <ajayrajthakur111@gmail.com>
2024-02-26 15:50:47 +0530

    UppercaseToLowercase

--- UppercaseToLowercase.js
+++ UppercaseToLowercase.js
@@ -1,3 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+ */
+
 let s= "HELlO";
 
 var toLowerCase = function(s) {
