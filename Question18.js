f6df2605848f0bdecdd763c5e0d124bf9848ef72
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:39 +0530

    Create README.md

---
+++ Question18.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 18: Retrieve the substring "new string" from the string "A new string with words." using the appropriate method.
+*/
+
+// Use the .substring() method
