02b5de8a8e47055cbc4e45988fbcb4527811842e
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:31 +0530

    Create README.md

---
+++ Question3.js
@@ -0,0 +1,9 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 3: Write a function that takes a number as input and returns the number formatted to have exactly two digits after the decimal point using the .toFixed() method.
+*/
+
+function formatToTwoDigits(num) {
+  // Write your code here
+}
+
