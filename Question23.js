a35b03af2958995836cb637deb222d5bf1fb6be6
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:42 +0530

    Create README.md

---
+++ Question23.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 23: Check if the string "A new string with words." includes the substring "string" using the correct method.
+*/
+
+// Use the .includes() method
