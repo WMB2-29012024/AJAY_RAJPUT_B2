954bcb6387e5b8e936567d3c22687fb4e9a4d959
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:47 +0530

    Create README.md

---
+++ Question33.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 33: Replace the first occurrence of the substring "l" with "z" in the string "Hello, World!".
+*/
+
+// Use the .replace() method with a string
