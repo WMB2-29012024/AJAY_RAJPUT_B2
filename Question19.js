126f4d308e769dff6412a0b58b17e18d57850929
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:40 +0530

    Create README.md

---
+++ Question19.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 19: Retrieve the substring "string with" from the string "A new string with words." using the correct method.
+*/
+
+// Use the .slice() method
