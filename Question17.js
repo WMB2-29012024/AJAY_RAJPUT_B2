a243075a8d59cc1c55759318801ce5c1a272fe34
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:39 +0530

    Create README.md

---
+++ Question17.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 17: Split the string "A new string with words." into an array of substrings with a limit of 3 and log the result.
+*/
+
+// Use the .split() method with a limit
