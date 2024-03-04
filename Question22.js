9288a5e09b873ed76b5ccfcf13430f66f1074f57
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:41 +0530

    Create README.md

---
+++ Question22.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 22: Check if the string "A new string with words." starts with the letter "A" using the appropriate method.
+*/
+
+// Use the .startsWith() method
