1fd5b9a61a5bc09239721bd202c6aee6aad91b63
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:37 +0530

    Create README.md

---
+++ Question13.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 13: Log the largest floating number by using Number.MAX_VALUE method and smallest floating number by using Number.MIN_VALUE method.
+*/
+
+
