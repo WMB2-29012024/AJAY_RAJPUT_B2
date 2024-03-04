f730a683ba66475802b63f7a221b568f001d8a68
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:38 +0530

    Create README.md

---
+++ Question16.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 16: Split the string "A new string with words." into an array of substrings separated by spaces and log the result.
+*/
+
+// Use the .split() method
