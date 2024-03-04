d06891155f4117023cb81abe62f5d93092218c96
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:40 +0530

    Create README.md

---
+++ Question20.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 20: Check if the string "A new string with words." ends with the word "words" using the appropriate method.
+*/
+
+// Use the .endsWith() method
