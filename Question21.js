74c430d3e1369f9233ae80f3fbd0a0c13d49336f
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:41 +0530

    Create README.md

---
+++ Question21.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 21: Check if the string "A new string with words." ends with the character "A" using the correct method.
+*/
+
+// Use the .endsWith() method
