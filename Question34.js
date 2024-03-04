7bb91aa9d206f4d2ce87b7a0c59527537ea374ae
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:48 +0530

    Create README.md

---
+++ Question34.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 34: Replace all occurrences of the substring "l" with "z" in the string "Hello, World!".
+*/
+
+// Use the .replaceAll() method with a string
