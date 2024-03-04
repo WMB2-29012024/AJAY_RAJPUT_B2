d47c85b84d3508a6a6a89a03708b3266eca87fff
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-04 20:16:46 +0530

    Create README.md

---
+++ Question31.js
@@ -0,0 +1,6 @@
+// Branch Name: B2_W6_D1
+      
+/* Question 31: Retrieve the Unicode code point of the character at index 0 from the string "Hello, World!".
+*/
+
+// Use the .codePointAt() method
