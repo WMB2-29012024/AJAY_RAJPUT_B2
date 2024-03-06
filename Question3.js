4ba2557c3625aee8c4a15ce4759cef0b5e1c977c
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-03-07 01:44:10 +0530

    Create README.md

---
+++ Question3.js
@@ -0,0 +1,12 @@
+// Branch Name: B2_W6_D3
+      
+/* Question 3: You're given a sorted array of integers. Write a function to replace all even numbers from the array with their squared values, using splice method. And then return all the numbers from the array which are greater than 6, in a new array using findIndex and slice method.
+*/
+
+
+// Input:
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Output:
+// [16, 36, 64, 100]
+
