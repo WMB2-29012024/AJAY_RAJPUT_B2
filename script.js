ef5e28761b548ff015c9ac509de820c9174b4bb6
InvincibleAj <ajayrajthakur111@gmail.com>
2024-04-02 16:17:33 +0530

    task1

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ formBtn.addEventListener("submit", (event) => {
   event.preventDefault();
 });
 inputelem.addEventListener("change", (event) => {
-    const data = document.createElement("li");
+    const data = document.createElement("p");
   inputelem.value = event.target.value;
   // console.log(inputelem.value);
-  data.innerText = inputelem.value;
+  data.innerText = `Hi ${inputelem.value}`;
   console.log(data);
   itemList.appendChild(data);
   console.log(itemList);
